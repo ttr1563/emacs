b09591fbe87ec9d6de46147af39cd75278fed04a
yamadamizuki <yamada2@skiyaki.com>
2017-07-13 10:41:32 +0900

    [emacs]設定ファイルのファーストコミット

---
+++ init.el
@@ -0,0 +1,87 @@
+;; 環境を日本語、UTF-8にする
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(set-locale-environment nil)
+(set-language-environment "Japanese")
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(prefer-coding-system 'utf-8-unix)
+
+;; ロックファイル / バックアップファイルを作成しない
+(setq create-lockfiles nil)
+(setq make-backup-files nil)
+(setq delete-auto-save-files t)
+
+;; 行の最後に来たら、新しい行を作らない
+(setq next-line-add-newlines nil)
+
+;; マウスで選択するとコピーする Emacs 24 ではデフォルトが nil
+(setq mouse-drag-copy-region t)
+
+;; 対応する括弧をハイライトする
+(show-paren-mode 1)
+
+;; 時間も表示させる。
+(display-time)
+
+;; 行番号を常に表示させる
+(global-linum-mode)
+(setq linum-format "%4d ")
+
+;; 現在行を目立たせる
+(setq hl-line-face 'underline)
+(global-hl-line-mode)
+
+
+;; 対応する括弧の色の設定
+(setq show-paren-style 'mixed)
+(set-face-background 'show-paren-match-face "grey")
+(set-face-foreground 'show-paren-match-face "black")
+
+;; カーソル位置の桁数をモードライン行に表示する
+(column-number-mode 1)
+
+;; カーソル位置の行数をモードライン行に表示する
+(line-number-mode 1)
+
+;; リージョンを色付きにする
+(transient-mark-mode 1)
+
+;; テーマの設定
+(package-initialize)
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("melpa" . "http://melpa.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")))
+(load-theme 'monokai t)
+
+
+;; カスタムセット
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (perspeen web-mode jsx-mode sass-mode emmet-mode multiple-cursors monokai-alt-theme package-utils monokai-theme undo-tree ##))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+;; web mode
+(setq web-mode-auto-close-style 1)
+(setq web-mode-tag-auto-close-style t)
